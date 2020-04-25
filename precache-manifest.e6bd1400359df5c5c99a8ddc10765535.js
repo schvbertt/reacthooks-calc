b8f22212520a45254a06e29547216cd20ca22a5a
schvbertt <diamenttx@gmail.com>
2020-04-25 13:47:57 +0200

    Updates

---
+++ precache-manifest.e6bd1400359df5c5c99a8ddc10765535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a4e9368f0de1aa9fbaa3f18d12b014",
+    "url": "/reacthooks-calc/index.html"
+  },
+  {
+    "revision": "b91880a3e349244587ee",
+    "url": "/reacthooks-calc/static/css/main.89015fe7.chunk.css"
+  },
+  {
+    "revision": "ad566bb72dc1ffea4470",
+    "url": "/reacthooks-calc/static/js/2.d7909971.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacthooks-calc/static/js/2.d7909971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b91880a3e349244587ee",
+    "url": "/reacthooks-calc/static/js/main.1a3a5aea.chunk.js"
+  },
+  {
+    "revision": "c2d7b2a57cc775a559a3",
+    "url": "/reacthooks-calc/static/js/runtime-main.d2b0cbe1.js"
+  }
+]);
